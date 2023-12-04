d62c395f61323430e8023f6a5cf698314fd3f028
Dunn, Simeon <simedunn01@gmail.com>
2023-12-04 14:17:17 -0500

    finished functionality

--- ChatGPTApp/ChatView.swift
+++ ChatGPTApp/ChatView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ChatView: View {
+    @ObservedObject var viewModel = ViewModel()
+    
     var body: some View {
         VStack {
-            Image(systemName: "house")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ScrollView {
+                ForEach(viewModel.messages.filter({$0.role != .system}), id: \.id) { message in
+                    messageView(message: message) 
+                }
+            }
+            HStack {
+                TextField("Enter a message... ", text: $viewModel.currentInput)
+                Button {
+                    viewModel.sendMessage()
+                } label: {
+                    Text("Send")
+                }
+            }
         }
         .padding()
+    }
+    
+    func messageView(message: Message) -> some View {
+        HStack {
+            if message.role == .user { Spacer() }
+            Text(message.content)
+            if message.role == .assistant { Spacer() }
+        }
     }
 }
 
--- ChatGPTApp/OpenAIService.swift
+++ ChatGPTApp/OpenAIService.swift
@@ -11,8 +11,17 @@ import Alamofire
 class OpenAIService {
     private let endpointURL = "https://api.openai.com/v1/chat/completions"
     
-    func sendMessage() {
+    func sendMessage(messages: [Message]) async -> OpenAIChatResponse? {
         
+        let openAIMessages = messages.map({OpenAIChatMessage(role: $0.role, content: $0.content)})
+        
+        let body = OpenAIChatBody(model: "gpt-3.5-turbo", messages: openAIMessages)
+        
+        let headers: HTTPHeaders = [
+            "Authorization": "Bearer \(Constants.openAIApiKey)"
+        ]
+        
+        return try? await AF.request(endpointURL, method: .post, parameters: body, encoder: .json, headers: headers).serializingDecodable(OpenAIChatResponse.self).value
     }
 }
 
@@ -21,13 +30,21 @@ struct OpenAIChatBody: Encodable {
     let messages: [OpenAIChatMessage]
 }
 
-struct OpenAIChatMessage: Encodable {
+struct OpenAIChatMessage: Codable {
     let role: SenderRole
     let content: String
 }
 
-enum SenderRole: String, Encodable {
+enum SenderRole: String, Codable {
     case system
     case user
     case assistant
+}
+
+struct OpenAIChatResponse: Decodable {
+    let choices: [OpenAIChatChoice]
+}
+
+struct OpenAIChatChoice: Decodable {
+    let message: OpenAIChatMessage
 }
