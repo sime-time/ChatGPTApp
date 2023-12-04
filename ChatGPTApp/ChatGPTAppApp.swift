a2729fc9f23efb900e9a453df1ab46aa964a0d03
Dunn, Simeon <simedunn01@gmail.com>
2023-12-04 12:10:22 -0500

    Initial Commit

---
+++ ChatGPTApp/ChatGPTAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTAppApp.swift
+//  ChatGPTApp
+//
+//  Created by Simeon on 12/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
